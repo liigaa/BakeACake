c4057cb6d1cba81a6f09e8b48b503d0bec579152
dainabo <daina.bogdanova@gmail.com>
2022-03-06 18:42:14 +0200

    main styling update

--- src/main/java/com/bakeacake/bakeacaketest/Main.java
+++ src/main/java/com/bakeacake/bakeacaketest/Main.java
@@ -2,6 +2,7 @@ package com.bakeacake.bakeacaketest;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
@@ -11,16 +12,27 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 700, 700);
+        Parent root = FXMLLoader.load(getClass().getResource("/view/login.fxml"));
+//        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/login.fxml"));
+//        Scene scene = new Scene(fxmlLoader.load(), 700, 700);
+        Scene scene = new Scene(root, 700, 700);
+//       scene.getStylesheets().add(getClass().getResource("/stylesheet.css").toExternalForm());
+//        scene.getStylesheets().add("/stylesheet.css");
+
+        String css = getClass().getResource("/view/stylesheet.css").toExternalForm();
+        scene.getStylesheets().clear();
+        scene.getStylesheets().add(css);
+
         stage.setTitle("Bake a Cake");
         Image icon = new Image(String.valueOf(Main.class.getResource("/images/favicon.png")));
         stage.getIcons().add(icon);
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
     public static void main(String[] args) {
         launch();
+
     }
 }
