10aad3d6a5cd174f4d94f5aed42de4ec3cfc8b03
Liga <liga.bandeniece@yahoo.com>
2022-03-14 22:09:19 +0200

    added ThankYouController and thank_you.fxml

---
+++ src/main/java/com/bakeacake/bakeacaketest/controller/ThankYouController.java
@@ -0,0 +1,17 @@
+package com.bakeacake.bakeacaketest.controller;
+
+import com.bakeacake.bakeacaketest.repository.DataManager;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ThankYouController extends ViewController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+
+}
